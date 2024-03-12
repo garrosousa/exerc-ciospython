e8254579d5979bc8f93b1a390d9ed0fa91d439e2
garrosousa <161743684+garrosousa@users.noreply.github.com>
2024-03-11 23:09:53 -0300

    exercicio 4 concluido

--- exercicio.py
+++ exercicio.py
@@ -18,9 +18,14 @@ tempo= int(input("tempo="))
 print("velocidade média=", distancia/tempo)
 
 #exercicio4
-a= float(input("valor de a="))
-b= float(input("valor de b="))
-c= float(input("valor de c="))
+a= int(input("valor de a="))
+b= int(input("valor de b="))
+c= int(input("valor de c="))
 delta = b**2 - 4*a*c
-x1 = (-b-(delta**0.5)/(2*a))
-x2 = (-b+(delta**0.5)/(2*a))
+x1 = ((-b)+ delta**(1/2))/(2*a)
+x2 = ((-b)- delta**(1/2))/(2*a)
+print(x1)
+print(x2)
+
+#exercicio5
+
